2aaf58e4c181a0e3b941b198c362cb2054828d9b
M Vishnu Kartik <EE25MT003@iitdh.ac.in>
2025-08-21 13:18:30 +0530

    Code for Sw11 for different colours of Led

--- main2.c
+++ main2.c
@@ -1,9 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
-/**
- * main.c
- */
+
 int main(void)
  {
     SYSCTL_RCGC2_R |=   0x00000020;
@@ -14,47 +12,80 @@ int main(void)
     GPIO_PORTF_PUR_R  = 0x10;
 
     GPIO_PORTF_DATA_R = 0x0;
-    long int x;
+    long int x,counter=0;
     long int delay = 675000;
 
 
-
-    // Set PF0 and PF4 as inputs
     GPIO_PORTF_DIR_R &= ~(0x11);
-   GPIO_PORTF_DEN_R |= 0x11;
+    GPIO_PORTF_DEN_R |= 0x11;
 
 
 
     while (1) {
-         if (((GPIO_PORTF_DATA_R & 0x01) == 0) && ((GPIO_PORTF_DATA_R & 0x10) == 0)) {
-                     GPIO_PORTF_DATA_R = 0x08; // Turn on Red LED (PF1)
-                     x=0;
-                                                                                    while ( x < delay)               // Delay
-                                                                                    x ++ ;}
-         else if ((GPIO_PORTF_DATA_R & 0x10) == 0) {
-                    GPIO_PORTF_DATA_R = 0x02; // Turn on Red LED (PF1)
+        if ((GPIO_PORTF_DATA_R & 0x01) == 0) {
+            if ((GPIO_PORTF_DATA_R & 0x01) == 0) {
+                counter++;
+
+
+
+                if (counter == 1) {
+                    GPIO_PORTF_DATA_R = 0x02;
                     x=0;
-                                                               while ( x < delay)               // Delay
-                                                               x ++ ;}
+                                                  while ( x < delay)
+                                                  x ++ ;
 
 
-            // Check if SW2 (PF0) is pressed
-        else if ((GPIO_PORTF_DATA_R & 0x01) == 0) {
-                GPIO_PORTF_DATA_R = 0x04; // Turn on Blue LED (PF2)
-                x=0;
-                                                                               while ( x < delay)               // Delay
-                                                                               x ++ ;
+                } else if (counter == 3) {
+                    GPIO_PORTF_DATA_R = 0x08;
+                    x=0;
+                                                  while ( x < delay)
+                                                  x ++ ;
 
-                                                                                                        }
-        else {
-            GPIO_PORTF_DATA_R = 0x00; // Turn off both LEDs
-            x=0;
-                               while ( x < delay)               // Delay
-                               x ++ ;
+                } else if (counter == 2) {
+                    GPIO_PORTF_DATA_R = 0x04;
+                    x=0;
+                                                                      while ( x < delay)
+                                                                      x ++ ;
 
-        }
+                }
+                else if (counter == 4) {
+                                    GPIO_PORTF_DATA_R = 0x0A;
+                                    x=0;
+                                                                  while ( x < delay)
+                                                                  x ++ ;
 
+                                }
+                else if (counter == 5) {
+                                    GPIO_PORTF_DATA_R = 0x0C;
+                                    x=0;
+                                                                  while ( x < delay)
+                                                                  x ++ ;
 
-    }
+                                }
+                else if (counter == 6) {
+                                                    GPIO_PORTF_DATA_R = 0x06;
+                                                    x=0;
+                                                                                  while ( x < delay)
+                                                                                  x ++ ;
+
+                                                }
+
+                else if (counter == 7) {
+                                    GPIO_PORTF_DATA_R = 0x0E;
+                                    x=0;
+                                                                  while ( x < delay)
+                                                                  x ++ ;
+
+                                }
+            }}
+            else {
+                                                                                                                                GPIO_PORTF_DATA_R = 0x00; // Turn off both LEDs
+                                                                                                                                x=0;
+                                                                                                                                                   while ( x < delay)               // Delay
+                                                                                                                                             x ++ ;}
+        if (counter > 7) {
+                           counter = 0;
+                       }
+
  }
-
+ }
